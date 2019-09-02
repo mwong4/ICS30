6f15bc34174c5bf068b94fde3e45f2832c321c01
MW <43390816+mwong4@users.noreply.github.com>
2019-09-02 13:48:51 -0400

    Added the ability to choose a custom coordinate

--- Armageddon 1.0.1/main.cpp
+++ Armageddon 1.0.1/main.cpp
@@ -1,29 +1,56 @@
+/*
+>- Author: Max Wong
+>- Date: Sep 1, 2019
+>- Updated: Sep 2, 2019
+>- Purpose: To write a game for a summative project.
+>- Game should incorperate all the major programming requirements from the course.
+>-
+>- [version 0.0.1]
+>- Thanks to Vedaant Srivastava for the error trapping system and play-testing
+>-
+>-Thanks to Thomas Maloley for teaching me how to program with C++
+>-
+>-
+>- [TO DO]
+>- commenting
+>- confirm + placing base
+>-
+*/
+
+//Declaring all used libraries
 #include <iostream>
 #include <fstream>
 #include <string>
 #include <stdlib.h>
 #include <windows.h>   // WinApi header
+#include <sstream>
+#include <iomanip>
+#include <math.h>
 
 using namespace std;
 
-void getMapFile (char[][55]);
-void saveMapFile (std::string, char[][55], int);
-void readMapArray (char[][55], HANDLE);
-void placeBase(char[][55], HANDLE);
+//Declaring all functions
+void getMapFile (char[][55]); //This function is used to read a txt file line by line
+void saveMapFile (std::string, char[][55], int); //This function is used to save the txt file into a double array
+void readMapArray (char[][55], HANDLE); //This function is used to print the map into the consol
+void placeBase(char[][55], HANDLE); //This function is used to select the location of a base
+
+bool stringChecker(string); //Function to check if an input is a float or integer
 
 int main()
 {
-    char gameMap [199][55];
+    //Declaring all variables
+    char gameMap [199][55];//This is the double array that houses the whole map
 
-    bool gameRun = true;
-    bool gameStart = true;
+    bool gameRun = true; //This boolean is used to determine if the program is running
+    bool gameStart = true; //This boolean is used to determine if the game is running for the first time
 
-    string inputCommand = "";
+    string inputCommand = "";//This variable is used to get the players input
 
     HANDLE hConsole;
-    hConsole = GetStdHandle(STD_OUTPUT_HANDLE);
+    hConsole = GetStdHandle(STD_OUTPUT_HANDLE); //HANDLE and hCOnsole are using the windows.h lbrary to color individual letters
 
-    //Reset the map double array
+    //Reset the map double array to blank/null
     for(int i = 0; i < 55; i++)
     {
         for(int j = 0; j < 199; j++)
@@ -32,26 +59,31 @@ int main()
         }
     }
 
-    //Get map from text file
+    //Get map from text file and save it
     getMapFile(gameMap);
 
+    //main loop of the game
     while(gameRun)
     {
+        //If the game is starting for the first time, print out the map
         if(gameStart)
         {
            readMapArray(gameMap, hConsole);
            gameStart = false;
         }
 
+        //Ask for a input from the player
         cout << ">- Please enter a direct command. Below are the primary listed commands." << endl;
         cout << "create a new base -> /spawn" << endl;
         cin >> inputCommand;
 
+        //If player enters /spawn, create a base
         if(inputCommand == "/spawn")
         {
             placeBase(gameMap, hConsole);
 
         }
+        //Otherwise, print an error message
         else if(inputCommand != "/spawn")
         {
             SetConsoleTextAttribute(hConsole, 12);
@@ -66,6 +98,7 @@ int main()
     return 0;
 }
 
+//This function is used to access the txt file and read the file line by line.
 void getMapFile(char gameMap[][55])
 {
     std::string line;
@@ -75,8 +108,10 @@ void getMapFile(char gameMap[][55])
 
     if(file_.is_open())
     {
+        //This function uses the builtin function: getline
         while(getline(file_,line))
         {
+            //It then references the saveMapFile function in order to save it into a doubel array.
             saveMapFile(line, gameMap, currentRow);
             currentRow += 1;
         }
@@ -85,8 +120,10 @@ void getMapFile(char gameMap[][55])
     return;
 }
 
+//Saves the map from the txt file into a double array
 void saveMapFile (std::string line, char gameMap [][55], int currentRow)
 {
+    //Running through every character
        for(std::string::size_type i = 0; i < line.size(); ++i)
     {
         //cout << line[i] << endl;
@@ -95,6 +132,7 @@ void saveMapFile (std::string line, char gameMap [][55], int currentRow)
     return;
 }
 
+//simply runs through a for loop through each value of the array and prints them out to console
 void readMapArray(char gameMap [][55], HANDLE hConsole)
 {
 
@@ -120,27 +158,36 @@ void readMapArray(char gameMap [][55], HANDLE hConsole)
     }
 }
 
+//Function used to place a base anywhere on the map
 void placeBase(char gameMap[][55], HANDLE hConsole)
 {
 
-    hConsole = GetStdHandle(STD_OUTPUT_HANDLE);
+    //Defining variables
 
-    bool choosingLocation = false;
-    bool errorOccured = false;
+    hConsole = GetStdHandle(STD_OUTPUT_HANDLE); //allows the manipulation of the console
 
-    string inputCommand = "";
+    bool choosingLocation = false; //boolean that is used to check if the player is still choosing a lcation
+    bool errorOccured = false; //Boolean that checks if an error has occured
+    bool findingCoordinate = false; //Boolean that checks if an error has occured
 
-    int current_x;
-    int current_y;
+    string inputCommand = ""; //Stirng to take inputs from player
 
-    char savedCharacter;
+    int current_x; //temporarily holds the x value
+    int current_y; //temporarily holds the y value
+
+    int saved_x; //Saves coordinate for the custom choice of coordinate feature
+    int saved_y;
+
+    char savedCharacter; //Temporarily saves a character
 
     cout << ">- Creating..." << endl << endl;
     choosingLocation = true;
 
+    //Spawn base on position:
     savedCharacter = gameMap[100][20];
     gameMap[100][20] = '@';
 
+    //save current x and current y
     current_x = 100;
     current_y = 20;
 
@@ -156,7 +203,7 @@ void placeBase(char gameMap[][55], HANDLE hConsole)
         {
 
             cout << ">- Move up -> /up" << endl << ">- Move down -> /down" << endl << ">- Move left -> /left" << endl << ">- Move right -> /right" << endl;
-            cout << endl << ">- Cancel -> /cancel" << endl;
+            cout << endl << ">- to place on a custom coordinate -> /input" << endl << ">- to place base -> /place" << endl << ">- Cancel -> /cancel" << endl;
         }
         errorOccured = false;
 
@@ -240,17 +287,89 @@ void placeBase(char gameMap[][55], HANDLE hConsole)
             }
         }
 
+        if(inputCommand == "/input")
+        {
+            findingCoordinate = true;
+
+            do{
+
+                cout << endl << ">- Please enter your specific X-coordinate below. Press /cancel to exit" << endl;
+                cin >> inputCommand;
+
+                if(inputCommand == "/cancel")
+                {
+                    findingCoordinate = false;
+                    inputCommand = "";
+                }
+
+                if( stringChecker(inputCommand))
+                {
+                    saved_x = round(::atof(inputCommand.c_str()));
+
+                    if(saved_x > 198) saved_x = 198;
+                    if(saved_x <  3) saved_x = 3;
+
+                    cout << endl << ">- Please enter your specific Y-coordinate below. Press /cancel to exit" << endl;
+                    cin >> inputCommand;
+
+                    if(inputCommand == "/cancel")
+                    {
+                        findingCoordinate = false;
+                        inputCommand = "";
+                    }
+                    if( stringChecker(inputCommand))
+                    {
+                        saved_y = round(::atof(inputCommand.c_str()));
+
+                        if(saved_y > 44) saved_y = 44;
+                        if(saved_y <  3) saved_y = 3;
+
+                        cout << ">- Your coordinate is [" << saved_x << "," << saved_y << "]. Press /y to confirm." << endl;
+                        cin >> inputCommand;
+
+                        if(inputCommand == "/y" || inputCommand == "/Y" || inputCommand == "/yes" || inputCommand == "/YES")
+                        {
+                            gameMap[current_x][current_y] = savedCharacter;
+                            savedCharacter = gameMap[saved_x][saved_y];
+                            gameMap[saved_x][saved_y] = '@';
+                            current_x = saved_x;
+                            current_y = saved_y;
+
+                            system("CLS");
+                            readMapArray(gameMap, hConsole);
+
+                            findingCoordinate = false;
+                            inputCommand  = "/input";
+                        }
+                    }
+                    else
+                    {
+                        cout << endl << ">- Error, please enter an integer between 2 and 45" << endl << endl;
+                    }
+                }
+                else
+                {
+                    cout << endl << ">- Error, please enter an integer between 2 and 199" << endl << endl;
+                }
+
+            }while(findingCoordinate);
+        }
+
         if(inputCommand == "/cancel" || inputCommand == "/CANCEL" || inputCommand == "/C" || inputCommand == "/c")
         {
             cout << endl << ">- You are Attempting to exit. Are you sure? Press /y to quit" << endl;
             cin >> inputCommand;
-            if(inputCommand == "y" || inputCommand == "Y" || inputCommand == "yes" || inputCommand == "YES")
+
+            system("CLS");
+            readMapArray(gameMap, hConsole);
+            if(inputCommand == "/y" || inputCommand == "/Y" || inputCommand == "/yes" || inputCommand == "/YES")
             {
-                  return;
+                return;
             }
         }
 
-        else if(inputCommand != "/up" && inputCommand != "/UP" && inputCommand != "/U" && inputCommand != "/u" && inputCommand != "/down" && inputCommand != "/DOWN" && inputCommand != "/D" && inputCommand != "/d" && inputCommand != "/left" && inputCommand != "/LEFT" && inputCommand != "/L" && inputCommand != "/l" && inputCommand != "/cancel" && inputCommand != "/CANCEL" && inputCommand != "/C" && inputCommand != "/c")
+        //If input is not valid, print an error message in red
+        else if(inputCommand != "/up" && inputCommand != "/UP" && inputCommand != "/U" && inputCommand != "/u" && inputCommand != "/down" && inputCommand != "/DOWN" && inputCommand != "/D" && inputCommand != "/d" && inputCommand != "/left" && inputCommand != "/LEFT" && inputCommand != "/L" && inputCommand != "/l" && inputCommand != "/cancel" && inputCommand != "/CANCEL" && inputCommand != "/C" && inputCommand != "/c" && inputCommand != "/input")
         {
             SetConsoleTextAttribute(hConsole, 12);
             cout << endl << "==================================================" << endl;
@@ -260,7 +379,31 @@ void placeBase(char gameMap[][55], HANDLE hConsole)
 
             errorOccured = true;
         }
-    }
 
+    }
+}
+
+//Function used to check if input string is a float
+//Credit to Vedaant for this function
+bool stringChecker(string myString)
+{
+    long double flMyString;
+    ostringstream conversion;
+
+    //Checks the string using built in iostream library
+    stringstream(myString) >> flMyString;
+    conversion << setprecision(9) << flMyString;
+
+    string convFlMyString = conversion.str();
+
+    //If initial string does not match corrected string, string is not a float
+    if (convFlMyString == myString)
+    {
+        return true;
+    }
+    else
+    {
+        return false;
+    }
 }
 
