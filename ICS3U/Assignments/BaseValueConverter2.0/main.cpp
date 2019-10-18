9b6eeaad0abcab4072bf0a4bd48c6b38da7b2570
MW <43390816+mwong4@users.noreply.github.com>
2019-10-17 21:10:16 -0400

    Finished the conversion program

--- ICS3U/Assignments/BaseValueConverter2.0/main.cpp
+++ ICS3U/Assignments/BaseValueConverter2.0/main.cpp
@@ -5,9 +5,6 @@
 >- Purpose: To write code that can convert bases
 >-
 >-Thanks to Thomas Maloley for teaching me how to program with C++
->-
->- TODO
-    //Up to hexadeciaml
 */
 
 //Defining Libraries
@@ -23,16 +20,18 @@
 using namespace std;
 
 //Declre all function prototypes
-int toBaseTen(int, float);
-void fromBaseTen(float, float);
+int toBaseTen(string, float);
+void fromBaseTen(int, float);
 int getAnswer(); //Function used to get the players response as an integer (with error trapping)
+string getString(); //This function gets a string that fits the constraint
 
 int main()
 {
-    float targetNumber = 0; //This is the number being converted;
+    int targetNumber = 0; //This is the number being converted;
     float baseNumber = 0; //This value is your base value
     float secondBase = 0; //The intended ending base number
     int tempInt = 0; //A temporary variable that stores an integer
+    string inputString = "";
 
     do
     {
@@ -43,13 +42,13 @@ int main()
         if(targetNumber == 1)
         {
             //Get player input for conversion value
-            cout << ">- Hello, please enter your /any/ base value" << endl;
-            tempInt = getAnswer();
+            cout << ">- Hello, please enter the number you would like to covert that is of /any/ base value" << endl;
+            inputString = getString();
             //Get player input for base value
             cout << ">- Now, define your base" << endl;
             baseNumber = getAnswer();
             //Convert the value to base 10
-            tempInt  = toBaseTen(tempInt, baseNumber);
+            tempInt  = toBaseTen(inputString, baseNumber);
 
             cout << "Answer: " << tempInt << endl; //Output answer
         }
@@ -69,7 +68,7 @@ int main()
         {
             //Get player input for value to be converted
             cout << ">- Hello, please enter the number you want to convert" << endl;
-            targetNumber = getAnswer();
+            inputString = getString();
             //Get input for original base
             cout << ">- Now, define your starting base" << endl;
             baseNumber = getAnswer();
@@ -77,7 +76,7 @@ int main()
             cout << ">- Now, define your ending base" << endl;
             secondBase = getAnswer();
             //Convert value to base ten before converting from base 10 to new base
-            tempInt = toBaseTen(targetNumber, baseNumber);
+            tempInt = toBaseTen(inputString, baseNumber);
             fromBaseTen(tempInt, secondBase);
             //Reset value to prevent premature quit
             targetNumber = 0;
@@ -94,28 +93,54 @@ int main()
     return 0; //Quit
 }
 
-int toBaseTen(int tempInt, float baseNumber)
+int toBaseTen(string inputValue, float baseNumber)
 {
     float targetNumber = 0; //A number that acts as the number to be converted
     int answerNumber = 0;
+    string tempString = "";
 
-    //Run for loop 100000 times
-    for(int i = 0; i < 100000; i ++)
+    //Run the loop to the size of the number to be converted
+    for(int i = (inputValue.size() - 1); i >= 0; i --)
     {
-        //Get the single digit
-        targetNumber =  tempInt % 10;
-        answerNumber += targetNumber * pow(baseNumber, i); //Comvert it to base 10
-        tempInt = floor(tempInt/10); //update the target number to one digit less
+        //Find the character/digit, starting from right to left. Save it into a temporary string
+        tempString = inputValue.at(i);
 
-        if(tempInt  <= 0) //If the input is 0, close loop
+        //If the valeus are hexadecimal, find the equivalent decimal value
+        if(tempString == "A")
         {
-            i = 100001;
+            targetNumber = 10;
         }
+        else if(tempString == "B")
+        {
+            targetNumber = 11;
+        }
+        else if(tempString == "C")
+        {
+            targetNumber = 12;
+        }
+        else if(tempString == "D")
+        {
+            targetNumber = 13;
+        }
+        else if(tempString == "E")
+        {
+            targetNumber = 14;
+        }
+        else if(tempString == "F")
+        {
+            targetNumber = 15;
+        }
+        else
+        {
+            //convert string to float
+            targetNumber = std::atoi (tempString.c_str());
+        }
+        answerNumber += targetNumber * pow(baseNumber, inputValue.size()-i-1); //Comvert it (float) to base 10 and add to answer
     }
     return answerNumber;
 }
 
-void fromBaseTen(float targetNumber, float baseNumber)
+void fromBaseTen(int targetNumber, float baseNumber)
 {
     float tempFloat = 0; //A temporary integer to convert to base greater than 9
     string answerString = ""; //This string saves your answer
@@ -147,10 +172,11 @@ void fromBaseTen(float targetNumber, float baseNumber)
             { answerString.insert(0,"*");
             }
         else //Otherwise insert it into a string
-            { answerString.insert(0, to_string(tempFloat));
+            { answerString.insert(0, to_string(llround(tempFloat)));
             }
         //Update the number to be converted
         targetNumber = floor(targetNumber/baseNumber);
+
     }while(targetNumber > (baseNumber-1));
 
     //in the end, convert the last remaining value and insert into string
@@ -182,4 +208,45 @@ int getAnswer ()
         }
     }while(findingInput);
     return playerInput;//Otherwise input is good, return input
+}
+
+//This function gets a string that fits the constraint
+string getString ()
+{
+    bool foundError; //This boolean is used to know if the value is not valid
+    bool isNumber; //This boolean is used to know if the value is not a number
+    string inputString = ""; //This string is what the user inputs
+    string tempString = ""; //Just a temporary string used to store the chosen character
+
+    do
+    {
+        //Start by assuming there is an error
+        foundError = false;
+        isNumber = false;
+        cout << ">- Please enter your number" << endl;
+        cin >> inputString;
+
+        for(int i = 0; i < inputString.size(); i++)
+        {
+            tempString = inputString.at(i);
+            cout << tempString;
+            for(int j = 0; j < 10; j++)
+            {
+                if (tempString == to_string(j))
+                {
+                    isNumber = true;
+                }
+            }
+            if(isNumber == false && tempString != "A" && tempString != "B" && tempString != "C" && tempString != "D" && tempString != "E" && tempString != "F")
+            {
+                foundError = true;
+            }
+        }
+        if(foundError == true)
+        {
+            cout << ">- [ERROR] Please enter values that are 1-9, A-F" << endl;
+        }
+    }while(foundError == true);
+
+    return inputString;
 }
--- ICS3U/Scraps/FindSpot/main.cpp
+++ ICS3U/Scraps/FindSpot/main.cpp
@@ -3,16 +3,73 @@
 
 using namespace std;
 
+string getString(); //This function gets a string that fits the constraint
+
 int main()
 {
-    string str = "Hello World";
+    /*
+    string str = "123456789";
+    string tempString = "";
     int stringSize = str.size();
 
     for(int i = 0; i < str.size(); i++)
     {
-        cout << stringSize << " - 1 - " << i << endl;
-        cout << i << ")  " << str.at(stringSize - i - 1) << endl;
+        cout << str.at(i) << endl;
+        cout <<  to_string(i) << endl;
+
+        //tempString = str.at(i);
+        if(tempString == to_string(i))
+       {
+           cout << "cool" << endl;
+       }
     }
+    */
+
+    string hi;
+
+    hi = getString();
 
     return 0;
 }
+
+string getString ()
+{
+    bool foundError; //This boolean is used to know if the value is not valid
+    bool isNumber; //This boolean is used to know if the value is not a number
+    string inputString = ""; //This string is what the user inputs
+    string tempString = ""; //Just a temporary string used to store the chosen character
+
+    do
+    {
+        //Start by assuming there is an error
+        foundError = false;
+        isNumber = false;
+        cout << ">- Please enter your number" << endl;
+        cin >> inputString;
+
+        for(int i = 0; i < inputString.size(); i++)
+        {
+            tempString = inputString.at(i);
+            cout << tempString;
+            for(int j = 0; j < 10; j++)
+            {
+                if (tempString == to_string(j))
+                {
+                    isNumber = true;
+                }
+            }
+            if(isNumber == false && tempString != "A" && tempString != "B" && tempString != "C" && tempString != "D" && tempString != "E" && tempString != "F")
+            {
+                foundError = true;
+            }
+        }
+        if(foundError == true)
+        {
+            cout << ">- [ERROR] Please enter values that are 1-9, A-F" << endl;
+        }
+        cout << "foundError " << foundError << endl;
+    }while(foundError == true);
+
+    return inputString;
+}
+
