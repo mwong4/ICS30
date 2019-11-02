c83cc0c0c40ca2ff2c06d9bf40a23351d1c87324
MW <43390816+mwong4@users.noreply.github.com>
2019-11-01 22:07:32 -0400

    Made RPSLS a lot more efficient

--- ICS3U/RockPaperScissorsLizzardSpoch/main.cpp
+++ ICS3U/RockPaperScissorsLizzardSpoch/main.cpp
@@ -1,56 +1,62 @@
+/*
+>- Author: Max Wong
+>- Date: Nov 1, 2019
+>- Updated: Nov 1, 2019
+>- Purpose: To create rock paper scissors lizard spock
+*/
+
 #include <iostream>
 
 using namespace std;
 
-bool didPlayerWin(int, int, int[]);
+bool didPlayerWin(int, int);
 
 int main()
 {
-    int decisionCombinations[10] = {5,1,2,4,3,5,2,4,3,1};
     int playerOneChoice;
     int playerTwoChoice;
 
-    while(true)
+    while(playerOneChoice != 9)
     {
         cout << ">- [1] Rock" << endl << ">- [2] Paper" << endl << ">- [3] Scissors" << endl << ">- [4] Lizzard" << endl << ">- [5] Spock" << endl << endl;
 
         cout << ">- [Player 1] Please pick a number to choose your choice" << endl;
         cin >> playerOneChoice;
 
-        playerTwoChoice = rand()%5+1;
-        cout << "AI: " << playerTwoChoice << endl;
+        playerTwoChoice = rand()%5;
+        cout << "AI: " << playerTwoChoice+1 << endl;
 
-        if(playerOneChoice == playerTwoChoice)
+        if(playerOneChoice-1 == playerTwoChoice)
         {
             cout << " Tie" << endl;
         }
-        else if(didPlayerWin(playerOneChoice, playerTwoChoice, decisionCombinations))
+        else if(didPlayerWin(playerOneChoice-1, playerTwoChoice))
         {
             cout << " Player One won" << endl;
         }
-        else if(didPlayerWin(playerTwoChoice, playerOneChoice, decisionCombinations))
+        else
         {
             cout << " Player Two won" << endl;
         }
 
+        cout << " Press 9 to quit" << endl;
         system("PAUSE");
         system("CLS");
     }
     return 0;
 }
 
-bool didPlayerWin(int inputOne, int inputTwo, int decisionCombinations[])
+bool didPlayerWin(int inputOne, int inputTwo)
 {
-    if(inputOne == 1 && inputTwo == 2 || inputOne == 1 && inputTwo == 5)
+    if(inputTwo == (inputOne + 2)%5 || inputTwo == (inputOne + 3)%5)
+    {
+        return true;
+    }
+    /*
+    else if(inputTwo == (inputOne + 1)%5 || inputTwo == (inputOne + 4)%5)
     {
         return false;
     }
-    else if(decisionCombinations[inputOne-1] == decisionCombinations[inputTwo-1]+1 || decisionCombinations[inputOne+4] == decisionCombinations[inputTwo+4]+1 )
-    {
-        return true;
-    }
-    else
-    {
-        return true;
-    }
+    */
+    return false;
 }
