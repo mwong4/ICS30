28a3950ec5e2289f0603b2a94a4177625ae885cf
MW <43390816+mwong4@users.noreply.github.com>
2019-11-05 09:33:34 -0500

    Cleaned up RPSLS a bit

--- ICS3U/RockPaperScissorsLizzardSpoch/main.cpp
+++ ICS3U/RockPaperScissorsLizzardSpoch/main.cpp
@@ -6,57 +6,75 @@
 */
 
 #include <iostream>
+//For error trapping
+#include<limits>
+#include <windows.h>   // WinApi header
 
 using namespace std;
 
-bool didPlayerWin(int, int);
+void didPlayerWin(int, int);
+float getAnswer(); //Function used to get the players response as an integer (with error trapping)
 
 int main()
 {
     int playerOneChoice;
     int playerTwoChoice;
 
-    while(playerOneChoice != 9)
+    while(playerOneChoice <= 5)
     {
         cout << ">- [1] Rock" << endl << ">- [2] Paper" << endl << ">- [3] Scissors" << endl << ">- [4] Lizzard" << endl << ">- [5] Spock" << endl << endl;
 
-        cout << ">- [Player 1] Please pick a number to choose your choice" << endl;
-        cin >> playerOneChoice;
+        cout << ">- [Player 1] Please pick a number to choose your choice" << endl << ">- Type 9 to quit" << endl;
+        playerOneChoice = getAnswer();
 
         playerTwoChoice = rand()%5;
-        cout << "AI: " << playerTwoChoice+1 << endl;
+        cout << ">- AI: " << playerTwoChoice+1 << endl;
 
-        if(playerOneChoice-1 == playerTwoChoice)
-        {
-            cout << " Tie" << endl;
-        }
-        else if(didPlayerWin(playerOneChoice-1, playerTwoChoice))
-        {
-            cout << " Player One won" << endl;
-        }
-        else
-        {
-            cout << " Player Two won" << endl;
-        }
+        didPlayerWin(playerOneChoice-1, playerTwoChoice)
 
-        cout << " Press 9 to quit" << endl;
         system("PAUSE");
         system("CLS");
     }
     return 0;
 }
 
-bool didPlayerWin(int inputOne, int inputTwo)
+void didPlayerWin(int inputOne, int inputTwo)
 {
-    if(inputTwo == (inputOne + 2)%5 || inputTwo == (inputOne + 3)%5)
+    if(inputOne == inputTwo)
     {
-        return true;
+        cout << " Tie" << endl;
     }
-    /*
-    else if(inputTwo == (inputOne + 1)%5 || inputTwo == (inputOne + 4)%5)
+    else if(inputTwo == (inputOne + 2)%5 || inputTwo == (inputOne + 3)%5)
     {
-        return false;
+        cout << " Player One won" << endl;
     }
-    */
-    return false;
+    else
+    {
+        cout << " Player Two won" << endl;
+    }
+    return;
+}
+
+//Get player input. Check to see if there is an error
+float getAnswer ()
+{
+    float playerInput; //This variable is used to get the player's input
+    bool findingInput; //This bool determines if the loop continues running
+
+    do
+    {
+        findingInput = false; //By default, the loop will end
+
+        cout << ">- Your input: "; //Get player input
+        cin >> playerInput;
+
+        if(cin.fail())//Check to see if player entered a "bad" input type
+        {
+            cin.clear(); //Clear all flags
+            cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n'); //ignore incorrect symbols
+            cout << endl << ">- [Error], please enter a number value!" << endl;
+            findingInput = true; //If the input is invalid, then the loop will loop
+        }
+    }while(findingInput);
+    return playerInput;//Otherwise input is good, return input
 }
